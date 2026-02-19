c3e5ee81190339432296c21705f2dd9a8872ff6f
bansalakshita137-glitch <bansalakshita137@gmail.com>
2026-02-19 17:05:49 +0530

    services.json file editted

--- app/src/main/java/com/example/bakery/BottomNavigationActivity.kt
+++ app/src/main/java/com/example/bakery/BottomNavigationActivity.kt
@@ -1,6 +1,7 @@
 package com.example.bakery
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,7 @@ class BottomNavigationActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Toast.makeText(this, "Hi", Toast.LENGTH_SHORT).show()
     }
 }
