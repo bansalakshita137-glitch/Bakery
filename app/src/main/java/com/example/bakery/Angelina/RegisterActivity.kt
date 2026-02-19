921af34dd7324fb8b601893ebb29746ca0419e4f
a04ngelina-ux <a04.ngelina@gmail.com>
2026-02-19 16:23:39 +0530

    register activity
    register activity

--- app/src/main/java/com/example/bakery/Angelina/RegisterActivity.kt
+++ app/src/main/java/com/example/bakery/Angelina/RegisterActivity.kt
@@ -1,21 +1,54 @@
 package com.example.bakery.Angelina
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.bakery.HomeActivity
 import com.example.bakery.R
+import com.example.bakery.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    var auth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.btnSubmit.setOnClickListener {
+            val email = binding.emailTV.text.toString()
+            val password = binding.passwordTV.text.toString()
+            if (email.isEmpty()) {
+                Toast.makeText(this, "Please fill email", Toast.LENGTH_SHORT).show()
+            } else if (password.isEmpty()) {
+                Toast.makeText(this, "Please fill password", Toast.LENGTH_SHORT).show()
+            } else {
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "Account created successfully!", Toast.LENGTH_SHORT)
+                            .show()
+                        val intent = Intent(this, HomeActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                    .addOnFailureListener { e->
+                        Toast.makeText(this, e.localizedMessage, Toast.LENGTH_SHORT).show()
+                    }
+            }
+
         }
     }
 }
